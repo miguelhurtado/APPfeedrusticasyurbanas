24e5079252da9ae1d41f7c9369c99c5b9bfd9e0e
Miguel Hurtado <miguel.hurtado@perianet.com>
2015-01-20 19:49:12 +0100

    añadido feed OK

--- feed.php
+++ feed.php
@@ -3,7 +3,7 @@
 $id = (int) $_REQUEST['id'];
 
 // Feed URL
-$feed_url = "http://www.rusticasyurbanas.com/2.0/index.php/respuestajbpoint/get/zoo/items?maxsubs=2&limit=10&orderby=id&orderdir=desc&catid=".$id;
+$feed_url = "http://www.rusticasyurbanas.com/2.0/index.php/respuestajbpoint/get/zoo/items?category=444&app=9&maxsubs=2&limit=10&orderby=id&orderdir=desc&catid=".$id;
 
 // Read the feed
 $handle = fopen($feed_url, "r");
